26bf317c1352dde6615de1f0ae8990c9631cf22a
Arthur Morais <mlkliso4000@gmail.com>
2026-03-20 16:14:01 -0300

    faz algo

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 export const filmes = [
   { id: '1', titulo: 'Duna: Parte 2', diretor: 'Denis Villeneuve', ano: '2024', imagem: 'https://www.europanet.com.br/upload/id_produto/107___/107350g.jpg' },
-  { id: '2', titulo: 'Oppenheimer', diretor: 'Christopher Nolan', ano: '2023' },
-  { id: '3', titulo: 'Alien: Romulus', diretor: 'Fede Álvarez', ano: '2024' },
-  { id: '4', titulo: 'Divertida Mente 2', diretor: 'Kelsey Mann', ano: '2024' },
-  { id: '5', titulo: 'Deadpool & Wolverine', diretor: 'Shawn Levy', ano: '2024' },
+  { id: '2', titulo: 'Oppenheimer', diretor: 'Christopher Nolan', ano: '2023', imagem: 'https://m.media-amazon.com/images/I/91L+jiIFA3L._AC_UF894,1000_QL80_.jpg' },
+  { id: '3', titulo: 'Alien: Romulus', diretor: 'Fede Álvarez', ano: '2024', imagem: 'https://m.media-amazon.com/images/I/614lcZY032L.jpg'},
+  { id: '4', titulo: 'Divertida Mente 2', diretor: 'Kelsey Mann', ano: '2024', imagem: 'https://m.media-amazon.com/images/S/pv-target-images/aa61ae8fb015160d802c4d5cb4fe6858058ea76485c3498ed9ff431eee4fc83f.jpg'},
+  { id: '5', titulo: 'Deadpool & Wolverine', diretor: 'Shawn Levy', ano: '2024', imagem: 'https://m.media-amazon.com/images/S/pv-target-images/dd6fb66997dd4cb5606b587bb255d2cd2cec20ecbd11852a8f6b07a1358d09a1.jpg' },
 ];
